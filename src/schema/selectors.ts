6a924a64d8ed992d1ee83be94557bdddd729eb60
Ritchie Anesco <ritchie.anesco@anz.com>
2020-04-02 07:21:14 +1100

    Remove redundant selectors and test

--- src/schema/selectors.ts
+++ src/schema/selectors.ts
@@ -4,9 +4,7 @@ import {
   JSONSchema7Type
 } from "json-schema";
 import get from "lodash/get";
-import isArray from "lodash/isArray";
 import nth from "lodash/nth";
-import has from "lodash/has";
 import findKey from "lodash/findKey";
 import { DEFINITION_ROOT } from "./constants";
 import { isSchemaObject } from "./types";
@@ -68,90 +66,6 @@ export const getPropertyType = (
 
 export const getRequired = (schema: JSONSchema7): JSONSchema7["required"] =>
   schema.required;
-
-/**
- * Return properties and required sub-schemas from the conditional objects(if)
- */
-
-export const getConditionProperties = (
-  condition: JSONSchema7,
-  key: string
-): false | JSONSchema7 => {
-  const { properties, required } = condition;
-  if (
-    (isSchemaObject(properties) && has(properties, key)) ||
-    (isArray(required) && required.includes(key))
-  ) {
-    return condition;
-  }
-  return false;
-};
-
-/**
- * Return the If sub-schema given a matching key
- */
-
-export const getIfCondition = (
-  schema: JSONSchema7 | undefined,
-  key: string
-): false | JSONSchema7 => {
-  if (!isSchemaObject(schema)) {
-    return false;
-  }
-  const condition = getConditions(schema);
-  if (isSchemaObject(condition[0])) {
-    return getConditionProperties(condition[0], key);
-  }
-  return false;
-};
-
-/**
- * Return the Then sub-schema given a matching key
- */
-
-export const getThenCondition = (
-  schema: JSONSchema7Definition | undefined,
-  key: string
-): false | JSONSchema7 => {
-  if (!isSchemaObject(schema)) {
-    return false;
-  }
-  const condition = getConditions(schema);
-  if (isSchemaObject(condition[1])) {
-    return getConditionProperties(condition[1], key);
-  }
-  return false;
-};
-
-/**
- * Return the Else sub-schema given a matching key
- */
-
-export const getElseCondition = (
-  schema: JSONSchema7Definition | undefined,
-  key: string
-): false | JSONSchema7 => {
-  if (!isSchemaObject(schema)) {
-    return false;
-  }
-  const condition = getConditions(schema);
-  if (isSchemaObject(condition[2])) {
-    return getConditionProperties(condition[2], key);
-  }
-  return false;
-};
-
-/**
- * Retrieve ALL conditional json sub-schema (if, then, else properties)
- */
-
-export const getConditions = (
-  schema: JSONSchema7
-): (JSONSchema7Definition | undefined)[] => [
-  schema.if,
-  schema.then,
-  schema.else
-];
 
 /**
  * Retrieve enums property value
